0cc1e823b159d50d6c7014679f38a99a8f2499fc
Tatsuika <tatu3939104@gmail.com>
2019-02-05 17:34:22 +0900

    xcodeのファイルの追加

---
+++ eeyann_front/eeyann_front/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eeyann_front
+//
+//  Created by 猪飼　立晟 on 2019/02/05.
+//  Copyright © 2019年 Tatsuika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

61fb94cb6ba47744633e33e367257e8b4e560e34
Tatsuika <tatu3939104@gmail.com>
2019-02-16 14:25:29 +0900

    プロジェクトの追加

---
+++ FrontDemo/FrontDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FrontDemo
+//
+//  Created by 猪飼　立晟 on 2019/02/16.
+//  Copyright © 2019年 Tatsuika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

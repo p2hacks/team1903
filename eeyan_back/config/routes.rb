2e60c806ba8beac21eab87d81dc808865d1e4d9c
daisuke sato <satodsuke@gmail.com>
2019-02-16 23:20:46 +0900

    postでdatabaseにデータを保存できるようにした

--- eeyan_back/config/routes.rb
+++ eeyan_back/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "chat/index" => "chatget#index"
+
+  post "chat/post" => "chatpost#room"
 end
